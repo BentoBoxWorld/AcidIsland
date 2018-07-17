c4b3c7d9a960c20c67db71a86975ffdad621bac5
tastybento <tastybento@wasteofplastic.com>
2018-07-16 19:00:49 -0700

    Added missing admin commands

--- src/main/java/bskyblock/addon/acidisland/commands/AcidCommand.java
+++ src/main/java/bskyblock/addon/acidisland/commands/AcidCommand.java
@@ -8,10 +8,12 @@ import us.tastybento.bskyblock.api.commands.CompositeCommand;
 import us.tastybento.bskyblock.api.user.User;
 import us.tastybento.bskyblock.commands.admin.AdminGetRankCommand;
 import us.tastybento.bskyblock.commands.admin.AdminInfoCommand;
+import us.tastybento.bskyblock.commands.admin.AdminRegisterCommand;
 import us.tastybento.bskyblock.commands.admin.AdminReloadCommand;
 import us.tastybento.bskyblock.commands.admin.AdminSchemCommand;
 import us.tastybento.bskyblock.commands.admin.AdminSetRankCommand;
 import us.tastybento.bskyblock.commands.admin.AdminTeleportCommand;
+import us.tastybento.bskyblock.commands.admin.AdminUnregisterCommand;
 import us.tastybento.bskyblock.commands.admin.AdminVersionCommand;
 import us.tastybento.bskyblock.commands.admin.teams.AdminTeamAddCommand;
 import us.tastybento.bskyblock.commands.admin.teams.AdminTeamDisbandCommand;
@@ -32,21 +34,24 @@ public class AcidCommand extends CompositeCommand {
         setParameters("commands.admin.help.parameters");
         setDescription("commands.admin.help.description");
         setWorld(((AcidIsland)getAddon()).getIslandWorld());
-        new AdminVersionCommand(this);
-        new AdminReloadCommand(this);
-        new AdminTeleportCommand(this, "tp");
-        new AdminTeleportCommand(this, "tpnether");
-        new AdminTeleportCommand(this, "tpend");
-        new AdminGetRankCommand(this);
-        new AdminSetRankCommand(this);
-        new AdminInfoCommand(this);
+
         // Team commands
         new AdminTeamAddCommand(this);
         new AdminTeamKickCommand(this);
         new AdminTeamDisbandCommand(this);
         new AdminTeamMakeLeaderCommand(this);
-        // Schems
+        // Other
+        new AdminGetRankCommand(this);
+        new AdminInfoCommand(this);
+        new AdminRegisterCommand(this);
+        new AdminReloadCommand(this);
         new AdminSchemCommand(this);
+        new AdminSetRankCommand(this);
+        new AdminTeleportCommand(this, "tp");
+        new AdminTeleportCommand(this, "tpnether");
+        new AdminTeleportCommand(this, "tpend");
+        new AdminUnregisterCommand(this);
+        new AdminVersionCommand(this);
     }
 
     @Override
