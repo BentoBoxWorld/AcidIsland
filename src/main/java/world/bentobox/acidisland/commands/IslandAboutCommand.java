38cb6aea7f15a218a12c1b6386f0e9ed644fcc0f
tastybento <tastybento@wasteofplastic.com>
2019-01-27 17:02:26 -0800

    Updated copyright year

--- src/main/java/world/bentobox/acidisland/commands/IslandAboutCommand.java
+++ src/main/java/world/bentobox/acidisland/commands/IslandAboutCommand.java
@@ -23,7 +23,7 @@ public class IslandAboutCommand extends CompositeCommand {
     @Override
     public boolean execute(User user, String label, List<String> args) {
         user.sendRawMessage("About " + getAddon().getDescription().getName() + " " + getAddon().getDescription().getVersion() + ":");
-        user.sendRawMessage("Copyright (c) 2017 - 2018 tastybento");
+        user.sendRawMessage("Copyright (c) 2017 - 2019 tastybento");
         user.sendRawMessage("See https://www.eclipse.org/legal/epl-2.0/ for license information.");
         return true;
     }
