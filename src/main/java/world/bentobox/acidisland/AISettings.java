f23071562533ade61590dc949f80d39fe8428445
tastybento <tastybento@wasteofplastic.com>
2019-06-07 22:20:04 -0700

    Cleans up kicked-keep-inventory config setting.
    https://github.com/BentoBoxWorld/BentoBox/issues/738

--- src/main/java/world/bentobox/acidisland/AISettings.java
+++ src/main/java/world/bentobox/acidisland/AISettings.java
@@ -303,8 +303,7 @@ public class AISettings implements WorldSettings {
     private boolean leaversLoseReset = false;
 
     @ConfigComment("Allow kicked players to keep their inventory.")
-    @ConfigComment("If false, kicked player's inventory will be thrown at the island leader if the")
-    @ConfigComment("kicked player is online and in the island world.")
+    @ConfigComment("Overrides the on-leave inventory reset for kicked players.")
     @ConfigEntry(path = "island.reset.kicked-keep-inventory")
     private boolean kickedKeepInventory = false;
 
@@ -713,6 +712,7 @@ public class AISettings implements WorldSettings {
     /**
      * @return the deathsSumTeam
      */
+    @Override
     public boolean isDeathsSumTeam() {
         return deathsSumTeam;
     }
@@ -752,12 +752,14 @@ public class AISettings implements WorldSettings {
     /**
      * @return the kickedKeepInventory
      */
+    @Override
     public boolean isKickedKeepInventory() {
         return kickedKeepInventory;
     }
     /**
      * @return the leaversLoseReset
      */
+    @Override
     public boolean isLeaversLoseReset() {
         return leaversLoseReset;
     }
