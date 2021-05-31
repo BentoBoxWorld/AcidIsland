4bfd78af19e9cc56b869fa21d4136283075384a8
tastybento <tastybento@wasteofplastic.com>
2021-05-31 16:13:20 -0700

    Add Pladdon

---
+++ src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
@@ -0,0 +1,12 @@
+package world.bentobox.acidisland;
+
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.Pladdon;
+
+public class AcidIslandPladdon extends Pladdon {
+
+    @Override
+    public Addon getAddon() {
+        return new AcidIsland();
+    }
+}
