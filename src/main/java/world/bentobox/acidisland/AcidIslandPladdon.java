3276813674bfaabd037af0ea5435df61d2744133
tastybento <tastybento@wasteofplastic.com>
2023-03-25 09:45:06 -0700

    Switch to use annotations for plugin.yml

--- src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
+++ src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
@@ -1,8 +1,15 @@
 package world.bentobox.acidisland;
 
+import org.bukkit.plugin.java.annotation.dependency.Dependency;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
+@Plugin(name="AcidIsland", version="1.0")
+@ApiVersion(ApiVersion.Target.v1_16)
+@Dependency(value = "BentoBox")
 public class AcidIslandPladdon extends Pladdon {
 
     @Override
