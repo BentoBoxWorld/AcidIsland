5362fc689e49fc559a370c61f7972506523c03d4
BONNe <bonne@bonne.id.lv>
2023-04-08 18:36:48 +0300

    Create plugin.yml (#135)
    * Create plugin.yml
    
    * Update pom.xml
    
    * Update AcidIslandPladdon.java

--- src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
+++ src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
@@ -1,13 +1,9 @@
 package world.bentobox.acidisland;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-@Plugin(name="AcidIsland", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_16)
 public class AcidIslandPladdon extends Pladdon {
 
     @Override
