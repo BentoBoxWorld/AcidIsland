3e728916eecf8b2648d2aefc64317fdf330cc521
tastybento <tastybento@users.noreply.github.com>
2024-07-20 07:48:19 -0700

    Return the Pladdon that was created

--- src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
+++ src/main/java/world/bentobox/acidisland/AcidIslandPladdon.java
@@ -2,12 +2,18 @@ package world.bentobox.acidisland;
 
 
 import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.GameModeAddon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 public class AcidIslandPladdon extends Pladdon {
 
+    private GameModeAddon addon;
+
     @Override
     public Addon getAddon() {
-        return new AcidIsland();
+        if (addon == null) {
+            addon = new AcidIsland();
+        }
+        return addon;
     }
 }
